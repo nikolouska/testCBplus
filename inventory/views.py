6b8abc243a8b2b92dfe0b1f7e284e3506c1c97c4
Nicolas Maret <nicolas.maret@epita.fr>
2020-06-22 15:45:00 +0200

    Inventory with link to item details

--- inventory/views.py
+++ inventory/views.py
@@ -1,9 +1,21 @@
-from django.shortcuts import render
+from django.shortcuts import get_object_or_404, render
+from django.template import loader
 
 # Create your views here.
 
 from django.http import HttpResponse
 
+from .models import Inventory
+
 
 def index(request):
-    return HttpResponse("Initial commit.")
+    latest_inventory_list = Inventory.objects.order_by('expiry_date')
+    template = loader.get_template('inventory/index.html')
+    context = {
+        'latest_inventory_list': latest_inventory_list,
+    }
+    return HttpResponse(template.render(context, request))
+
+def detail(request, id):
+    item = get_object_or_404(Inventory, pk=id)
+    return render(request, 'inventory/detail.html', {'item': item})
